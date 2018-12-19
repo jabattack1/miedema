44458343d04c917e4239ff7d0e0f6cacaa0d50cd
jabattack1 <peter.hahn@live.com>
2018-12-19 20:16:59 +0900

    loop

--- scripts.js
+++ scripts.js
@@ -20,3 +20,75 @@ $(function() {
     }
   });
 });
+
+
+var movie1= "<img id='moviea' src='https://imgur.com/uLezNcX.gif' title='source: imgur.com' id='action' />"
+var movie2= "<img id='movieb' src='https://imgur.com/I3mBEQM.gif' title='source: imgur.com' id='action' />"
+var movie3= "<img id='moviec' src='https://imgur.com/8ObB1O1.gif' title='source: imgur.com' id='action' />"
+var movie4= "<img id='movied' src='https://imgur.com/ylAuB8g.gif' title='source: imgur.com' id='action' />"
+
+
+
+$(document).ready(function() {
+	$('#media').append(movie1);
+	$('#media').append(movie2);
+	$('#media').append(movie3);
+	$('#media').append(movie4);
+	$('#moviea').hide();
+	$('#movieb').hide();
+	$('#moviec').hide();
+	$('#movied').hide();
+
+	setTimeout(function() {
+    	$('#moviea').show();
+  	}, 0);
+  	setTimeout(function() {
+    	$('#moviea').hide();
+    }, 10000);
+    setTimeout(function() {
+    	$('#movieb').show();
+    }, 10001);
+    setTimeout(function() {
+    	$('#movieb').hide();
+    }, 20000);
+    setTimeout(function() {
+    	$('#moviec').show();
+    }, 20001);
+    setTimeout(function() {
+    	$('#moviec').hide();
+   	}, 30000);
+    setTimeout(function() {
+    	$('#movied').show();
+    }, 30001);
+    setTimeout(function() {
+    	$('#movied').hide();
+   	}, 40000);
+
+	setInterval(function() {
+		setTimeout(function() {
+    		$('#moviea').show();
+  		}, 0);
+  		setTimeout(function() {
+    		$('#moviea').hide();
+    	}, 10000);
+    	setTimeout(function() {
+    		$('#movieb').show();
+    	}, 10001);
+    	setTimeout(function() {
+    		$('#movieb').hide();
+    	}, 20000);
+    	setTimeout(function() {
+    		$('#moviec').show();
+    	}, 20001);
+    	setTimeout(function() {
+    		$('#moviec').hide();
+   		}, 30000);
+    	setTimeout(function() {
+    		$('#movied').show();
+    	}, 30001);
+    	setTimeout(function() {
+    		$('#movied').hide();
+   		}, 40000);
+    }, 40000);
+
+});
