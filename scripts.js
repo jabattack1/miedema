58df18866db2c873807a5a2da901468f7300f1dc
jabattack1 <peter.hahn@live.com>
2018-12-23 01:04:17 +0900

    slow movie

--- scripts.js
+++ scripts.js
@@ -4,74 +4,74 @@
 //     }, 2000);
 // });
 
-var movie1= "<img id='moviea' src='https://imgur.com/uLezNcX.gif' title='source: imgur.com' id='action' />"
-var movie2= "<img id='movieb' src='https://imgur.com/I3mBEQM.gif' title='source: imgur.com' id='action' />"
-var movie3= "<img id='moviec' src='https://imgur.com/8ObB1O1.gif' title='source: imgur.com' id='action' />"
-var movie4= "<img id='movied' src='https://imgur.com/ylAuB8g.gif' title='source: imgur.com' id='action' />"
+var movie1= "<img id='moviea' src='https://i.imgur.com/uLezNcX.gif' />"
+// var movie2= "<img id='movieb' src='https://media.giphy.com/media/5nmNO5Fn9mTVkoBm8F/giphy.gif' />"
+// var movie3= "<img id='moviec' src='https://media.giphy.com/media/9DvczI5xVK8pVsce5B/giphy.gif' />"
+// var movie4= "<img id='movied' src='https://media.giphy.com/media/YlyOEhIGKvjXwh76bB/giphy.gif' />"
 
 
 
 $(document).ready(function() {
 	$('#media').append(movie1);
-	$('#media').append(movie2);
-	$('#media').append(movie3);
-	$('#media').append(movie4);
 	$('#moviea').hide();
-	$('#movieb').hide();
-	$('#moviec').hide();
-	$('#movied').hide();
+	// $('#media').append(movie2);
+ //   	$('#movieb').hide();
+ //   	$('#media').append(movie3);
+ //   	$('#moviec').hide();
+ //   	$('#media').append(movie4);
+ //    $('#movied').hide();
 
 	setTimeout(function() {
     	$('#moviea').show();
   	}, 0);
-  	setTimeout(function() {
-    	$('#moviea').hide();
-    }, 10000);
-    setTimeout(function() {
-    	$('#movieb').show();
-    }, 10001);
-    setTimeout(function() {
-    	$('#movieb').hide();
-    }, 20000);
-    setTimeout(function() {
-    	$('#moviec').show();
-    }, 20001);
-    setTimeout(function() {
-    	$('#moviec').hide();
-   	}, 30000);
-    setTimeout(function() {
-    	$('#movied').show();
-    }, 30001);
-    setTimeout(function() {
-    	$('#movied').hide();
-   	}, 40000);
+ //  	setTimeout(function() {
+ //    	$('#moviea').hide();
+ //    }, 10000);
+ //    setTimeout(function() {
+ //    	$('#movieb').show();
+ //    }, 10001);
+ //    setTimeout(function() {
+ //    	$('#movieb').hide();
+ //    }, 20000);
+ //    setTimeout(function() {
+ //    	$('#moviec').show();
+ //    }, 20001);
+ //    setTimeout(function() {
+ //    	$('#moviec').hide();
+ //   	}, 30000);
+ //    setTimeout(function() {
+ //    	$('#movied').show();
+ //    }, 30001);
+ //    setTimeout(function() {
+ //    	$('#movied').hide();
+ //   	}, 40000);
 
-	setInterval(function() {
-		setTimeout(function() {
-    		$('#moviea').show();
-  		}, 0);
-  		setTimeout(function() {
-    		$('#moviea').hide();
-    	}, 10000);
-    	setTimeout(function() {
-    		$('#movieb').show();
-    	}, 10001);
-    	setTimeout(function() {
-    		$('#movieb').hide();
-    	}, 20000);
-    	setTimeout(function() {
-    		$('#moviec').show();
-    	}, 20001);
-    	setTimeout(function() {
-    		$('#moviec').hide();
-   		}, 30000);
-    	setTimeout(function() {
-    		$('#movied').show();
-    	}, 30001);
-    	setTimeout(function() {
-    		$('#movied').hide();
-   		}, 40000);
-    }, 40000);
+	// setInterval(function() {
+	// 	setTimeout(function() {
+ //    		$('#moviea').show();
+ //  		}, 0);
+ //  		setTimeout(function() {
+ //    		$('#moviea').hide();
+ //    	}, 10000);
+ //    	setTimeout(function() {
+ //    		$('#movieb').show();
+ //    	}, 10001);
+ //    	setTimeout(function() {
+ //    		$('#movieb').hide();
+ //    	}, 20000);
+ //    	setTimeout(function() {
+ //    		$('#moviec').show();
+ //    	}, 20001);
+ //    	setTimeout(function() {
+ //    		$('#moviec').hide();
+ //   		}, 30000);
+ //    	setTimeout(function() {
+ //    		$('#movied').show();
+ //    	}, 30001);
+ //    	setTimeout(function() {
+ //    		$('#movied').hide();
+ //   		}, 40000);
+ //    }, 40000);
 
 });
 
