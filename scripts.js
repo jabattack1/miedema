b2da532f662735270afd577c894a347044a46c60
jabattack1 <peter.hahn@live.com>
2018-12-24 19:22:16 +0900

    cubs

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@
 //     }, 2000);
 // });
 
-var movie1= "<img id='moviea' src='https://i.imgur.com/LIaEGuk.gif' />"
-// var movie2= "<img id='movieb' src='https://media.giphy.com/media/5nmNO5Fn9mTVkoBm8F/giphy.gif' />"
-// var movie3= "<img id='moviec' src='https://media.giphy.com/media/9DvczI5xVK8pVsce5B/giphy.gif' />"
-// var movie4= "<img id='movied' src='https://media.giphy.com/media/YlyOEhIGKvjXwh76bB/giphy.gif' />"
+var movie1= "<iframe width='1000' height='700' src='https://www.youtube.com/embed/EKeTpxznWe8?autoplay=1&&mute=1&controls=0&loop=1&showinfo=0&playlist=EKeTpxznWe8' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+// var movie2= "<img id='movieb' src='https://i.imgur.com/J6sjDIU.gif' />"
+// var movie3= "<img id='moviec' src='https://i.imgur.com/Yy5iWrE.gif' />"
+// var movie4= "<img id='movied' src='https://i.imgur.com/Ix3pFXW.gif' />"
 
 
 
@@ -21,9 +21,9 @@ $(document).ready(function() {
  //   	$('#media').append(movie4);
  //    $('#movied').hide();
 
-	setTimeout(function() {
-    	$('#moviea').show();
-  	}, 0);
+	// setTimeout(function() {
+ //    	$('#moviea').show();
+ //  	}, 0);
  //  	setTimeout(function() {
  //    	$('#moviea').hide();
  //    }, 10000);
